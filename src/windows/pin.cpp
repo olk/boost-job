922cfcad0b13338b2f32530715e3c475538369f1
Oliver Kowalke <oliver.kowalke@gmx.de>
2015-08-11 18:54:37 +0200

    fix pin_thread() for Windows

--- src/windows/pin.cpp
+++ src/windows/pin.cpp
@@ -10,6 +10,7 @@ extern "C" {
 #include <windows.h>
 }
 
+#include <cstring>
 #include <system_error>
 
 #ifdef BOOST_HAS_ABI_HEADERS
@@ -23,17 +24,18 @@ namespace detail {
 BOOST_JOBS_DECL
 void pin_thread( uint32_t cpuid) {
     GROUP_AFFINITY affinity;
+    std::memset( & affinity, 0, sizeof( affinity) );
     // compute processor group
     // a group contains max 64 logical CPUs
     affinity.Group = cpuid / 64;
     // compute the ID of the logical CPU in the group
-    uint32_t id = cpuid % 64 + 1; 
+    uint32_t id = cpuid % 64; 
     // set the bit mask of the logical CPU
     affinity.Mask = static_cast< KAFFINITY >( 1) << id;
-    if ( 0 == ::SetThreadAffinityMask( ::GetCurrentThread(), ( DWORD_PTR) & affinity) ) {
+    if ( 0 == ::SetThreadGroupAffinity( ::GetCurrentThread(), & affinity, nullptr) ) {
         throw std::system_error(
                 std::error_code( ::GetLastError(), std::system_category() ),
-                "::SetThreadAffinityMask() failed");
+                "::SetThreadiGroupAffinity() failed");
     }
 }
 
